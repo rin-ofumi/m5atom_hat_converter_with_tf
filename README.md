471adcbaa8d0d4497a4bc6193795c15031cd54ac
rin_ofumi <57037180+rin-ofumi@users.noreply.github.com>
2023-12-20 20:01:21 +0900

    M5Unified使用に変更。

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # hat_converter_with_tf
-Arduino project / HAT Converter with TF / for M5Atom Lite/Matrix
+Arduino project / HAT Converter with TF / for M5Atom Lite/M5Atom Matrix/M5AtomS3/M5AtomS3 Lite
 
 <br>
 
 # <概要>
 
-M5Atom Lite/Matrix用「HAT Converter with TF」のサンプルプログラムです。（[「HAT Converter with TF」はBOOTHにて販売中](https://kitto-yakudatsu.booth.pm/items/2385119)）<br>
+M5Atom Lite/M5Atom Matrix/M5AtomS3/M5AtomS3 Lite用「HAT Converter with TF」のサンプルプログラムです。（[「HAT Converter with TF」はBOOTHにて販売中](https://kitto-yakudatsu.booth.pm/items/2385119)）<br>
 
 <br>
 
@@ -13,11 +13,17 @@ M5Atom Lite/Matrix用「HAT Converter with TF」のサンプルプログラム�
 
 ## 「sd_test.ino」
 SDカードの種類・容量・ファイル一覧をシリアル出力するArduinoスケッチです。<br>
-M5Atom Lite/Matrixどちらにも使えます。
+M5Atom Lite/M5Atom Matrix/M5AtomS3/M5AtomS3 Liteで使用出来ます。
 
 <br>
 
 # <アップデート履歴>
+
+## 【2023.12.20】
+
+* M5Unified使用に変更、M5Atom系/M5AtomS3系の機種判別追加。
+
+<br>
 
 ## 【2020.9.19】
 
--- sd_test.ino
+++ sd_test.ino
@@ -1,39 +1,85 @@
-#include <M5Atom.h>
-#include "SD.h"
+// M5Atom Lite/Matrix/ATOMS3/ATOMS3 Lite用
+// 「HAT Converter with TF」のサンプルプログラム
+// @rin_ofumi 2023.12.20
+//
+// 確認した機種
+// - M5Atom Lite
+// - M5Atom Matrix
+// - M5ATOMS3
+// - M5ATOMS3 Lite
+//
+// 開発環境
+// - Arduino IDE ver2.2.1
+//
+// ボードマネージャバージョン
+// - M5Stack 2.0.9
+//
+// 使用ライブラリバージョン
+// - M5Unified 0.1.11
 
-SPIClass SPI2;
+#include <SD.h>
+#include <M5Unified.h>
+
+SPIClass SPI_EXT;
 
 void setup() {
-  M5.begin(true,false,true);
+  M5.begin();
 
-  //SDカード用SPIポート初期化（SCK:G23, MISO:G33, MOSI:G19, SS:物理ピン無し"-1"）
-  SPI2.begin(23, 33, 19, -1);
+  //機種判定（ATOM系とATOMS3系の区別）
+  const char* name;
+  switch (M5.getBoard()) {
+    case m5::board_t::board_M5AtomS3Lite:   name = "ATOMS3Lite";  break;
+    case m5::board_t::board_M5AtomS3:       name = "ATOMS3";      break;
+    case m5::board_t::board_M5Atom:         name = "ATOM";        break;
+    default:                                name = "Who am I ?";  break;
+  }
+  Serial.printf("> board name = [%s]\n", name);
+
+  //画面向き指定（逆は”3”）
+  M5.Lcd.setRotation(1);
+
+  //機種判定に応じ、ピン割り当てを変更
+  if (name == "ATOMS3" or name == "ATOMS3Lite") {
+    //ATOMS3系 SDカード用SPIポート初期化（SCK:G7, MISO:G8, MOSI:G6, SS:物理ピン無し"-1"）
+    SPI_EXT.begin(7, 8, 6, -1);
+  } else if (name == "ATOM") {
+    //ATOM系 SDカード用SPIポート初期化（SCK:G23, MISO:G33, MOSI:G19, SS:物理ピン無し"-1"）
+    SPI_EXT.begin(23, 33, 19, -1);
+  }
 
   //SDカード初期設定
-  if (!SD.begin(-1, SPI2)) {
-    Serial.println("SD Card Mount Failed!");
+  if (!SD.begin(-1, SPI_EXT, 15000000)) {
+    M5.Lcd.println("SD Card Mount Failed!");
+    Serial.println("> SD Card Mount Failed!");
     return;
   }
 
   //SDカード種別取得
   uint8_t cardType = SD.cardType();
   if (cardType == CARD_NONE) {
-    Serial.println("None SD Card!");
+    M5.Lcd.println("None SD Card!");
+    Serial.println("> None SD Card!");
     return;
   } else if (cardType == CARD_MMC) {
-    Serial.println("SD Card Type: MMC");
+    M5.Lcd.println("SD Card Type: MMC");
+    Serial.println("> SD Card Type: MMC");
   } else if (cardType == CARD_SD) {
-    Serial.println("SD Card Type: SDSC");
+    M5.Lcd.println("SD Card Type: SDSC");
+    Serial.println("> SD Card Type: SDSC");
   } else if (cardType == CARD_SDHC) {
-    Serial.println("SD Card Type: SDHC");
+    M5.Lcd.println("SD Card Type: SDHC");
+    Serial.println("> SD Card Type: SDHC");
   } else {
-    Serial.println("SD Card Type: UNKNOWN");
+    M5.Lcd.println("SD Card Type: UNKNOWN");
+    Serial.println("> SD Card Type: UNKNOWN");
   }
 
   //SDカード容量取得
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  Serial.printf("SD Card Size: %lluMB\n", cardSize);
-  Serial.println();
+  M5.Lcd.printf("SD Card Size: %lluMB\n", cardSize);
+  M5.Lcd.println();
+  Serial.printf("> SD Card Size: %lluMB\n", cardSize);
+  Serial.print("\n");
 
   //SDカードのルートディレクトリを開く
   File dir = SD.open("/");
@@ -44,6 +90,7 @@ void setup() {
     if (!entry) {
       break;
     }
+    M5.Lcd.println(entry.name());
     Serial.println(entry.name());
     entry.close();
   }
